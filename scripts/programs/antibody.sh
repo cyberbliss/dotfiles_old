36558db737f5d6b518147cf85b51b30bf078275f
Steve Judd <stephen.judd@gmail.com>
2022-04-18 15:08:18 +0000

    Marked scripts as executables

--- scripts/programs/antibody.sh
+++ scripts/programs/antibody.sh
@@ -1,3 +1,3 @@
-curl --remote-name https://github.com/getantibody/antibody/releases/download/v6.1.1/antibody_6.1.1_linux_amd64.deb
+curl -LO https://github.com/getantibody/antibody/releases/download/v6.1.1/antibody_6.1.1_linux_amd64.deb
 sudo apt install -y ./antibody_6.1.1_linux_amd64.deb
 rm -rf antibody_6.1.1_linux_amd64.deb
