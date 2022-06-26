5c61ecdc3f7ee569482f0dcbba981dc95d669d8f
Steve Judd <steve.judd@jetstack.io>
2022-06-26 17:50:48 +0100

    Fix version typo

--- scripts/programs/shelby.sh
+++ scripts/programs/shelby.sh
@@ -1,7 +1,7 @@
 _arch=$(dpkg --print-architecture)
 curl -LO https://github.com/cyberbliss/shelby/releases/download/v2.0.1/shelby_2.0.1_Linux_${_arch}.tar.gz
-tar -xvzf shelby_2.0.0_Linux_${_arch}.tar.gz
+tar -xvzf shelby_2.0.1_Linux_${_arch}.tar.gz
 sudo mv shelby /usr/local/bin/shelby
-rm -rf shelby_2.0.0_Linux_${_arch}.tar.gz
+rm -rf shelby_2.0.1_Linux_${_arch}.tar.gz
 rm -rf LICENSE
 rm -rf README.md
